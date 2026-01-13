d5909d9bb5de0e9b7846df3434f65913a6eca74a
Basenkokarolina <karolinabas0ma@mail.ru>
2026-01-13 19:26:20 +0300

    first commit1

--- config.py
+++ config.py
@@ -9,4 +9,4 @@ class Config:
     MYSQL_DB = 'flower_site2'
     SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    PERMANENT_SESSION_LIFETIME = timedelta(days=31)
+    PERMANENT_SESSION_LIFETIME = timedelta(days=30)
